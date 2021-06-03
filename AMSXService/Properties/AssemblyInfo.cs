53d39b63ccb96aa6e6c3a6586a2a0f61dc056107
EmrullahYilmazz <korater583@gmail.com>
2021-06-03 22:36:03 +0300

    Web API oluşturuldu ve bağlantısı kuruldu

---
+++ AMSXService/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("AMSXService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AMSXService")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğini false olarak ayarlarsanız COM bileşenleri bu 
+// derlemedeki türleri göremez. Bu derlemedeki bir türe COM'dan erişmeniz 
+// gerekiyorsa, o türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("7dc4b2e7-2aa7-4141-8614-9fb92d23483e")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilirsiniz veya Düzeltme ve Yapı Numaralarına aşağıda 
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AMSXService/service.asmx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.IO;
+using System.Xml.Serialization;
+using System.Data;
+using System.Text;
+
+namespace AMSXService
+{
+    /// <summary>
+    /// service için özet açıklama
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Bu Web Hizmeti'nin, ASP.NET AJAX kullanılarak komut dosyasından çağrılmasına, aşağıdaki satırı açıklamadan kaldırmasına olanak vermek için.
+    // [System.Web.Script.Services.ScriptService]
+    public class service : System.Web.Services.WebService
+    {
+        private DataSet1TableAdapters.KullaniciTableAdapter taAllvids = new DataSet1TableAdapters.KullaniciTableAdapter();
+        private DataSet1.KullaniciDataTable dtAllvids;
+       
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public void getAllData()
+        {
+            Context.Response.Write(getvids());
+        }
+
+        private string getvids()
+        {
+            dtAllvids = taAllvids.Getvits();
+            string myResponse = Newtonsoft.Json.JsonConvert.SerializeObject(dtAllvids);
+            return myResponse;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Xml)]
+        public void getAlldataXML()
+        {
+            Context.Response.Write(getvidsXML());
+        }
+
+        private string getvidsXML()
+        { 
+            string myReturn = string.Empty;
+            dtAllvids = taAllvids.Getvits();
+            using (var memoryStream = new MemoryStream())
+            {
+                using (TextWriter streamWriter = new StreamWriter(memoryStream))
+                {
+                    var xmlSerialiser = new XmlSerializer(typeof(DataTable));
+                    xmlSerialiser.Serialize(streamWriter, dtAllvids);
+                    myReturn = Encoding.UTF8.GetString(memoryStream.ToArray());
+                }
+            }
+
+            return myReturn;
+
+        }
+    }
+}
